47ac642f93c165360fbdf690f12bd531be8c7d6b
William Páez <paez.william8@gmail.com>
2021-08-11 05:29:06 -0400

    Actualizado a django 3.2

--- sofi/settings.default.py
+++ sofi/settings.default.py
@@ -38,10 +38,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
-import os
+from pathlib import Path
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -113,7 +113,7 @@ WSGI_APPLICATION = 'sofi.wsgi.application'
 DATABASES = {
     # 'default': {
     #   'ENGINE': 'django.db.backends.sqlite3',
-    #   'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #   'NAME': BASE_DIR / 'db.sqlite3',
     # }
 
     'default': {
@@ -180,11 +180,11 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = BASE_DIR / 'media'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static/'),
-    os.path.join(BASE_DIR, 'media/'),
+    BASE_DIR / 'static/',
+    BASE_DIR / 'media/',
 )
 
 LOGIN_URL = 'user:login'
@@ -209,9 +209,7 @@ else:
 # Configuración del CAPTCHA
 # Ruta en donde se encuentra el diccionario de palabras a utilizar en la
 # generación del captcha
-CAPTCHA_WORDS_DICTIONARY = os.path.join(
-    BASE_DIR, 'static/dictionaries/captcha-es.txt'
-)
+CAPTCHA_WORDS_DICTIONARY = BASE_DIR / 'static/dictionaries/captcha-es.txt'
 
 # Establece el tipo de captcha a generar. Se establece a la extraccion de
 # palabras a partir de un diccionario
@@ -240,3 +238,8 @@ if DEBUG:
     # Tiempo de expiración del captcha en entornos de desarrollo, representado
     # en minutos
     CAPTCHA_TIMEOUT = 1440  # 24 horas
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
